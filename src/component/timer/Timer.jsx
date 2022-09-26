46c86450a154fab492cc5f76af7bf34a97014a58
JI3BA <lyovkin_artyom@mail.ru>
2022-09-26 20:38:21 +0300

    second try created timer

--- src/component/timer/Timer.jsx
+++ src/component/timer/Timer.jsx
@@ -2,29 +2,32 @@ import React , { useState, useEffect } from "react";
 import './Timer.css'
 
 const Timer = () => {
-    const [hours, setHours] = useState(0)
-    const [minutes, setMinutes] = useState('00')
-    const [seconds, setSeconds] = useState('40')
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+    const [mseconds, setMseconds] = useState(0)
 
     useEffect(() => {
-    //    setInterval(() => {
-    //     if(seconds < 9){
-    //         setSeconds('0' + String(Number(seconds) + 1))
-    //     }else if(seconds >= 9 && seconds < 60){
-    //         setSeconds(String(Number(seconds) + 1))
-    //     }else if(seconds >= 60){
-    //         setSeconds('00')
-    //         setMinutes('0' + String(Number(minutes) + 1))
-    //     }
-    //    }, 1000)
-    }, [hours, minutes, seconds])
+        let addTimer = setTimeout(function tick(){
+            setMseconds(mseconds + 1)
+            if(mseconds === 99){
+                setMseconds(0)
+                setSeconds(seconds + 1)
+            }
+            if(seconds === 59){
+                setSeconds(0)
+                setMinutes(minutes + 1)
+            }
+            addTimer = setTimeout(tick, 1)
+        }, 1)
 
+        return () => clearTimeout(addTimer)
+    }, [mseconds, minutes, seconds])
 
     return(
         <div className="timer-container">
-            <p className="timer timer-hours">{hours}<span>:</span></p>
-            <p className="timer timer-minutes">{minutes}<span>:</span></p>
-            <p className="timer timer-seconds">{seconds}</p>
+            <p className="timer timer-minutes">{minutes < 10 ? '0' + minutes : minutes}<span>:</span></p>
+            <p className="timer timer-seconds">{seconds < 10 ? '0' + seconds : seconds}<span>:</span></p>
+            <p className="timer timer-mseconds">{mseconds < 10 ? '0' + mseconds : mseconds}</p>
         </div>
     )
 }
