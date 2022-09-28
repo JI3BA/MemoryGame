c3b5e3bdb604a8afb58125ba84a73a2df2ad520f
JI3BA <lyovkin_artyom@mail.ru>
2022-09-28 19:07:34 +0300

    third try create stopwatch

--- src/component/timer/Timer.jsx
+++ src/component/timer/Timer.jsx
@@ -2,32 +2,26 @@ import React , { useState, useEffect } from "react";
 import './Timer.css'
 
 const Timer = () => {
-    const [minutes, setMinutes] = useState(0)
-    const [seconds, setSeconds] = useState(0)
-    const [mseconds, setMseconds] = useState(0)
-
+    const [time, setTime] = useState(0);
+    const [active, setActive] = useState(false);
+    
     useEffect(() => {
-        let addTimer = setTimeout(function tick(){
-            setMseconds(mseconds + 1)
-            if(mseconds === 99){
-                setMseconds(0)
-                setSeconds(seconds + 1)
-            }
-            if(seconds === 59){
-                setSeconds(0)
-                setMinutes(minutes + 1)
-            }
-            addTimer = setTimeout(tick, 1)
-        }, 1)
-
-        return () => clearTimeout(addTimer)
-    }, [mseconds, minutes, seconds])
+        let interval;
+        if (active) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        }else if(!active) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [active]);
 
     return(
         <div className="timer-container">
-            <p className="timer timer-minutes">{minutes < 10 ? '0' + minutes : minutes}<span>:</span></p>
-            <p className="timer timer-seconds">{seconds < 10 ? '0' + seconds : seconds}<span>:</span></p>
-            <p className="timer timer-mseconds">{mseconds < 10 ? '0' + mseconds : mseconds}</p>
+            <p className="timer timer-minutes">{("0" + Math.floor((time / 60000) % 60)).slice(-2)}<span>:</span></p>
+            <p className="timer timer-seconds">{("0" + Math.floor((time / 1000) % 60)).slice(-2)}<span>:</span></p>
+            <p className="timer timer-mseconds">{("0" + ((time / 10) % 100)).slice(-2)}</p>
         </div>
     )
 }
