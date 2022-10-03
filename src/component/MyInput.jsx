c2010a3411dd21a306f6db3f4ed62a3d1697bf04
JI3BA <lyovkin_artyom@mail.ru>
2022-10-03 23:58:37 +0300

    thinking about validation form

--- src/component/MyInput.jsx
+++ src/component/MyInput.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState} from "react";
+import { useEffect } from "react";
 import '../styles/MyInput.css'
 
 const MyInput = () => {
+    const [changeValue, setChangeValue] = useState({value: ''})
+
+    const changeName = (e) => {
+        setChangeValue({value: e.target.value})
+    }
+
+    useEffect(() => {
+        if(changeName.value === ''){
+
+        }
+    }, [changeName])
+
+
     return (
-        <input type="text" className="input" placeholder="Nickname" maxLength={15} />
+        <input type="text" className="input" placeholder="Nickname" maxLength={15} value={changeValue.value} onChange={changeName}/>
     )
 }
 
--- src/component/pages/LogIn/LogIn.jsx
+++ src/component/pages/LogIn/LogIn.jsx
@@ -19,6 +19,7 @@ const LogIn = () => {
         }
     }
 
+
     return (
         <div className="container">
             <div className="login-container">
